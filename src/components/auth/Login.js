21f5ce9f5cc15524e98e16fbf2f7f1922449a5c0
cavesa10 <cavesa10@gmail.com>
2020-08-03 00:04:15 -0500

    Creando login/sign up

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,9 +1,67 @@
-import React from 'react'
+import React, {useState} from "react";
+import {Link} from 'react-router-dom'
 
 export const Login = () => {
+
+  const [loginValidacion, setLoginValidacion] = useState({
+    email: '',
+    password: ''
+  })
+
+  const {email, password} = loginValidacion
+
+  const handleOnChange = (e) => {
+    
+    setLoginValidacion({
+      ...loginValidacion,
+      [e.target.name]: e.target.value
+    })
+
+  };
+
+  const handleOnSubmit = e  => {
+    e.preventDefault()
+
+    // Validación que no haya campos vacios
+
+    // Pasarlos al action
+  }
+
   return (
     <div>
-      <h2>Desde el login</h2>
+      <div className="form-usuario">
+        <div className="contenedor-form sombra-dark">
+          <h1>Iniciar Sesión</h1>
+          <form onSubmit={handleOnSubmit}>
+            <div className="campo-form">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="Tu Email"
+                value={email}
+                onChange={handleOnChange}
+              />
+            </div>
+            <div className="campo-form">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Tu Password"
+                value={password}
+                onChange={handleOnChange}
+              />
+            </div>
+            <div className="campo-form">
+              <input type="submit" value="Iniciar Sesión" className="btn btn-primario btn-block" />
+            </div>
+          </form>
+          <Link to={'/nueva-cuenta'} className="enlace-cuenta" > Crear Cuenta </Link>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
--- src/components/auth/NuevaCuenta.js
+++ src/components/auth/NuevaCuenta.js
@@ -1,9 +1,95 @@
-import React from 'react'
+import React, {useState} from "react";
+import {Link} from 'react-router-dom'
 
 export const NuevaCuenta = () => {
+
+  const [loginValidacion, setLoginValidacion] = useState({
+    nombre: '',
+    email: '',
+    password: '',
+    confirmar: ''
+  })
+
+  const {nombre, email, password, confirmar} = loginValidacion
+
+  const handleOnChange = (e) => {
+    
+    setLoginValidacion({
+      ...loginValidacion,
+      [e.target.name]: e.target.value
+    })
+
+  };
+
+  const handleOnSubmit = e  => {
+    e.preventDefault()
+
+    // Validación que no haya campos vacios
+
+    // password minimo de 6 caracteres
+
+    // los dos pass sean iguales
+
+    // Pasarlos al action
+  }
+
   return (
     <div>
-      <h2>Desde Nueva Cuenta</h2>
+      <div className="form-usuario">
+        <div className="contenedor-form sombra-dark">
+          <h1>Crear Cuenta</h1>
+          <form onSubmit={handleOnSubmit}>
+          <div className="campo-form">
+              <label htmlFor="nombre">Nombre</label>
+              <input
+                type="text"
+                id="nombre"
+                name="nombre"
+                placeholder="Tu Nombre"
+                value={nombre}
+                onChange={handleOnChange}
+              />
+            </div>
+            <div className="campo-form">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="Tu Email"
+                value={email}
+                onChange={handleOnChange}
+              />
+            </div>
+            <div className="campo-form">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Tu Password"
+                value={password}
+                onChange={handleOnChange}
+              />
+            </div>
+            <div className="campo-form">
+              <label htmlFor="confirmar">Confirmar Password</label>
+              <input
+                type="password"
+                id="confirmar"
+                name="confirmar"
+                placeholder="Repite tu Password"
+                value={confirmar}
+                onChange={handleOnChange}
+              />
+            </div>
+            <div className="campo-form">
+              <input type="submit" value="Resgistar" className="btn btn-primario btn-block" />
+            </div>
+          </form>
+          <Link to={'/'} className="enlace-cuenta" > Volver a Iniciar Sesión </Link>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
