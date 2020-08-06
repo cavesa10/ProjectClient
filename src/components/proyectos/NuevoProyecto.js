5440f59525799f07057ccf3cfa0947dffc138fa8
cavesa10 <cavesa10@gmail.com>
2020-08-06 00:02:12 -0500

    Añadiendo useReducer

--- src/components/proyectos/NuevoProyecto.js
+++ src/components/proyectos/NuevoProyecto.js
@@ -1,52 +1,63 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useContext } from "react";
+
+import { proyectoContext } from "../../context/proyectos/proyectoContext";
 
 export const NuevoProyecto = () => {
+  // obtener el state del formulario
+  const proyectosContext = useContext(proyectoContext);
+  const { formulario, mostrarFormulario } = proyectosContext;
 
   // state Agregar Proyecto
   const [proyecto, setProyecto] = useState({
-    nombre: ''
-  })
+    nombre: "",
+  });
 
-  const {nombre} = proyecto
+  const { nombre } = proyecto;
 
-  const handleOnChange = e => {
+  const handleOnChange = (e) => {
     setProyecto({
       ...proyecto,
-      [e.target.name]: e.target.value
-    })
-  }
+      [e.target.name]: e.target.value,
+    });
+  };
 
   const handleSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     // Validar el proyecto
 
     // Agregar al state
-    
+
     // Reinciar el state
-    setProyecto({nombre: ''})
+    setProyecto({ nombre: "" });
+  };
+
+  const handleClick = () => {
+    mostrarFormulario()
   }
 
   return (
     <Fragment>
-      <button type="button" className="btn btn-primario btn-block">
+      <button type="button" className="btn btn-primario btn-block" onClick={ handleClick} >
         Nuevo Proyecto
       </button>
 
-      <form onSubmit={handleSubmit} className="formulario-nuevo-proyecto">
-        <input
-          type="text"
-          className="input-text"
-          placeholder="Nombre Proyecto"
-          name="nombre"
-          value={nombre}
-          onChange={handleOnChange}
-        />
-        <input
-          type="submit"
-          className="btn btn-primario btn-block"
-          value="Agregar Proyecto"
-        />
-      </form>
+      {formulario ? (
+        <form onSubmit={handleSubmit} className="formulario-nuevo-proyecto">
+          <input
+            type="text"
+            className="input-text"
+            placeholder="Nombre Proyecto"
+            name="nombre"
+            value={nombre}
+            onChange={handleOnChange}
+          />
+          <input
+            type="submit"
+            className="btn btn-primario btn-block"
+            value="Agregar Proyecto"
+          />
+        </form>
+      ) : null}
     </Fragment>
   );
 };
--- src/context/proyectos/ProyectoState.js
+++ src/context/proyectos/ProyectoState.js
@@ -2,6 +2,7 @@ import React, {useReducer} from 'react'
 
 import {proyectoContext} from './proyectoContext'
 import proyectoReducer from './proyectoReducer'
+import {FORMULARIO_PROYECTO} from '../../types'
 
 export const ProyectoState = props => {
   const initialState = {
@@ -13,11 +14,18 @@ export const ProyectoState = props => {
   const [state, dispatch] = useReducer(proyectoReducer, initialState)
 
   // serie de funciones para el CRUD
+  const mostrarFormulario = () => {
+    dispatch({
+      type: FORMULARIO_PROYECTO
+    })
+  }
+
 
   return (
     <proyectoContext.Provider
       value= {{
-        formulario: state.formulario
+        formulario: state.formulario,
+        mostrarFormulario
       }}
     >
       {props.children}
